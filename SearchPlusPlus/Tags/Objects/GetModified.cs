a4b3867082db6ecd43668287546aee99336b8663
Balint817 <pecsibalint2@gmail.com>
2026-03-24 00:34:18 +0100

    add GetModified

---
+++ SearchPlusPlus/Tags/Objects/GetModified.cs
@@ -0,0 +1,32 @@
+﻿using CustomAlbums.Managers;
+using Il2CppAssets.Scripts.Database;
+using Il2CppAssets.Scripts.PeroTools.Commons;
+using Il2CppAssets.Scripts.PeroTools.GeneralLocalization;
+using Il2CppPeroPeroGames.GlobalDefines;
+
+namespace IronSearch.Tags
+{
+    internal partial class BuiltIns
+    {
+
+        internal static DateTime? GetModified(MusicInfo musicInfo)
+        {
+            if (!EvalCustom(musicInfo))
+            {
+                return null;
+            }
+            return GetModifiedInternal(musicInfo);
+        }
+        internal static DateTime GetModifiedInternal(MusicInfo musicInfo)
+        {
+            var album = AlbumManager.LoadedAlbums.Values.First(x => x.Uid == musicInfo.uid);
+            return File.GetLastWriteTimeUtc(album.Path);
+        }
+        internal static dynamic EvalGetModified(SearchArgument M, dynamic[] varArgs, Dictionary<string, dynamic> varKwargs)
+        {
+            ThrowIfNotEmpty(varArgs);
+            ThrowIfNotEmpty(varKwargs);
+            return GetModified(M.I)!;
+        }
+    }
+}
