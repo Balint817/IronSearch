c4f4248a695c01e3a902e922cf9332725c8b2cc7
Balint817 <Balint817@users.noreply.github.com>
2026-03-29 12:41:43 +0200

    add romanization helper

---
+++ IronSearch/RomanizationHelper.cs
@@ -0,0 +1,57 @@
+﻿using Romanization;
+using System.Collections.ObjectModel;
+
+namespace IronSearch
+{
+    public static class RomanizationHelper
+    {
+        static readonly Dictionary<string, ReadOnlyCollection<string>> _cache = new();
+        public static ReadOnlyCollection<string> GetAllRomanizations(string input)
+        {
+            if (_cache.TryGetValue(input, out var cached))
+                return cached;
+
+            var results = new HashSet<string>();
+
+            try
+            {
+                var processed = new Japanese.ModifiedHepburn();
+                results.Add(processed.Process(input));
+            }
+            catch { }
+
+            try
+            {
+                var processed = new Japanese.KanjiReadings();
+                results.Add(processed.Process(input));
+            }
+            catch { }
+
+            try
+            {
+                var processed = new Chinese.HanyuPinyin();
+                results.Add(processed.Process(input));
+            }
+            catch { }
+
+            try
+            {
+                var processed = new Korean.RevisedRomanization();
+                results.Add(processed.Process(input));
+            }
+            catch { }
+
+            try
+            {
+                var processed = new Korean.HanjaReadings();
+                results.Add(processed.Process(input));
+            }
+            catch { }
+
+            cached = new(results.ToArray());
+            _cache.TryAdd(input, cached);
+
+            return cached;
+        }
+    }
+}
