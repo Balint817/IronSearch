bff6d51189d21dd0af5b5a2b3553b4b193cc87bd
Balint817 <pecsibalint2@gmail.com>
2026-04-04 01:45:12 +0200

    bump version

--- IronSearch/Properties/MelonModInfo.cs
+++ IronSearch/Properties/MelonModInfo.cs
@@ -8,7 +8,7 @@
 
         public const string Author = "PBalint817";
 
-        public const string Version = "0.3.1";
+        public const string Version = "0.3.2";
 
         public const string DownloadLink = "";
 
